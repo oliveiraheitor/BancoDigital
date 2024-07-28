1cccea194b16bf682c54b0d5cf27b71c062f57c0
oliveiraheitor <heitor.ooliveira29@gmail.com>
2024-07-27 23:05:44 -0300

    first commit

---
+++ src/Cliente.java
@@ -0,0 +1,15 @@
+public class Cliente {
+        private String nome;
+     
+        public Cliente() {
+        }
+     
+        public String getNome() {
+           return this.nome;
+        }
+     
+        public void setNome(String nome) {
+           this.nome = nome;
+        }
+     }
+     
